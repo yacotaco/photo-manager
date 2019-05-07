f93a53c2e2234e09baa9a49a9655e3632e39cac1
yacotaco <kamilkurach@yahoo.pl>
2019-05-07 07:06:02 +0200

    Add open listener and print file content to console

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,6 +1,13 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFileChooser;
+import javax.lang.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 /**
  * Read
  */
@@ -14,7 +21,8 @@ public class Controller {
     }
 
     public void initControler(){
-        view.getExitItem().addActionListener(new ExitActionListener());
+        view.getMenuExitItem().addActionListener(new ExitActionListener());
+        view.getMenuOpenItem().addActionListener(new OpenActionListener());
     }
 
     public class ExitActionListener implements ActionListener {
@@ -28,7 +36,33 @@ public class Controller {
 
     }
 
-    
+    public class OpenActionListener implements ActionListener {
+      final JFileChooser fc = new JFileChooser();
+      String path;
 
+      public OpenAction() {};
+
+      @Override
+      public void actionPerformed(ActionEvent event) {
+        
+        int returnVal = fc.showOpenDialog(view.getFrame());
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            path = fc.getSelectedFile().getPath();
+            readFile(path);
+        }
+      
+      }  
+    }
+
+    public void readFile(String path) {
+      try (Stream<String> stream = Files.lines(Paths.get(path))) {
+
+        stream.forEach(System.out::println);
+  
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
 }
 
